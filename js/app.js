589e801f28db50991cae7aac7f087f7ee9041eed
Ellyn Howard <ellynhoward@gmail.com>
2015-12-18 09:16:18 -0800

    used linter to fix syntax errors

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@
 
 var allProducts=[];
 var resultTable = document.getElementById('listOfProducts');
-var productNames = ['bag','banana','boots','chair','cthulhu','dragon','pen','scissors','shark','unicorn','water_can','wine_glass']
+var productNames = ['bag','banana','boots','chair','cthulhu','dragon','pen','scissors','shark','unicorn','water_can','wine_glass'];
 
 var chartData = {
   labels:[],
   datasets:[
     {
-  label:"ImagesVote",
-  fillColor: "rgba(210,30,20,0.5)",
-  strokeColor: "rgba(120,20,120,0.8)",
-  highlightFill: "rgba(20,210,220,0.75)",
-  highlightStroke: "rgba(240,223,220,1)",
-  data:[]
-  }
-]};
+      label:'ImagesVote',
+      fillColor: 'rgba(210,30,20,0.5)',
+      strokeColor: 'rgba(120,20,120,0.8)',
+      highlightFill: 'rgba(20,210,220,0.75)',
+      highlightStroke: 'rgba(240,223,220,1)',
+      data:[]
+    }
+  ]};
 function Product(name, path){//define constructor instead of object literal
   this.name = name;
   this.path = path;
@@ -33,7 +33,7 @@ function buildAlbum(){//short way instead of call 14 object seperately
 buildAlbum();//call the function
 // -----------------------------------------------------------------------
 
- var productRank = {
+var productRank = {
   totalVotes:0,
   leftObj:null,
   midObj:null,
@@ -44,56 +44,56 @@ buildAlbum();//call the function
   rightEl: document.getElementById('imgThree'),
   ctx:document.getElementById('chart').getContext('2d'),
   getRandomIndex: function() {
-      return Math.floor(Math.random() * productNames.length)
-    },
+    return Math.floor(Math.random() * productNames.length);
+  },
   displayImages: function() {
-  this.leftObj = allProducts[productRank.getRandomIndex()];
-  this.rightObj = allProducts[productRank.getRandomIndex()];
-  this.midObj = allProducts[productRank.getRandomIndex()];
+    this.leftObj = allProducts[productRank.getRandomIndex()];
+    this.rightObj = allProducts[productRank.getRandomIndex()];
+    this.midObj = allProducts[productRank.getRandomIndex()];
 
-  if(productRank.leftObj===productRank.midObj || productRank.leftObj===productRank.rightObj || productRank.midObj===productRank.rightObj) {
-    productRank.displayImages();
-   }
-   productRank.leftEl.src = productRank.leftObj.path;
-   productRank.leftEl.id = productRank.leftObj.name;
+    if(productRank.leftObj===productRank.midObj || productRank.leftObj===productRank.rightObj || productRank.midObj===productRank.rightObj) {
+      productRank.displayImages();
+    }
+    productRank.leftEl.src = productRank.leftObj.path;
+    productRank.leftEl.id = productRank.leftObj.name;
 
-   productRank.middleEl.src = productRank.midObj.path;
-   productRank.middleEl.id = productRank.midObj.name;
+    productRank.middleEl.src = productRank.midObj.path;
+    productRank.middleEl.id = productRank.midObj.name;
 
-   productRank.rightEl.src = productRank.rightObj.path;
-   productRank.rightEl.id = productRank.rightObj.name;
+    productRank.rightEl.src = productRank.rightObj.path;
+    productRank.rightEl.id = productRank.rightObj.name;
   },
 
   showResults: function() {
     if(this.totalVotes%15 === 0){
       document.getElementById('button').hidden = false;
       resultTable.hidden = false;
-      }
+    }
     else {
       document.getElementById('button').hidden = true;
       removeTable();
-        }
-    },
+    }
+  },
   showChart: function() {
     var newJsonValue=[];
-      if(localStorage.productVotes){
-        var jsonValue = localStorage.getItem('productVotes');
-        var returnJson = JSON.parse(jsonValue);
-        for (var i = 0; i < returnJson.length; i++){
+    if(localStorage.productVotes){
+      var jsonValue = localStorage.getItem('productVotes');
+      var returnJson = JSON.parse(jsonValue);
+      for (var i = 0; i < returnJson.length; i++){
         newJsonValue[i] = returnJson[i] + allProducts[i].tally;
         chartData.datasets[0].data[i] =  newJsonValue[i];
         localStorage.setItem('productVotes', JSON.stringify(newJsonValue));
-        }
-      }else {
-        var jsonData=[];
-        for (var i = 0; i < allProducts.length; i++){
+      }
+    }else {
+      var jsonData=[];
+      for (var i = 0; i < allProducts.length; i++){
         chartData.datasets[0].data[i] =  allProducts[i].tally;
         jsonData[i] =  allProducts[i].tally;
       }
-        localStorage.setItem('productVotes', JSON.stringify(jsonData));
-      }
-        new Chart(this.ctx).Bar(chartData);
+      localStorage.setItem('productVotes', JSON.stringify(jsonData));
     }
+    new Chart(this.ctx).Bar(chartData);
+  }
 };
 
 // -----------------------------------------------------------------------
@@ -132,15 +132,15 @@ productRank.displayImages();
 // -------------------------------------------------------------------------
 //a function to create a table for results
 function productTable(){
-  resultTable.style.border="1px solid black";
+  resultTable.style.border='1px solid black';
   resultTable.hidden = false;
   var thEl1 = document.createElement('th');
   var thEl2 = document.createElement('th');
   var trEl = document.createElement('tr');
 
-  thEl1.textContent="Products";
+  thEl1.textContent='Products';
   trEl.appendChild(thEl1);
-  thEl2.textContent="Votes";
+  thEl2.textContent='Votes';
   trEl.appendChild(thEl2);
   resultTable.appendChild(trEl);
   for (var i = 0; i < allProducts.length; i++) {
@@ -152,10 +152,10 @@ function productTable(){
     tdEl.textContent = allProducts[i].tally;
     trEl.appendChild(tdEl);
     resultTable.appendChild(trEl);
-  };
+  }
 }
 function removeTable() {
-while (resultTable.firstChild) {
+  while (resultTable.firstChild) {
     resultTable.removeChild(resultTable.firstChild);
   }
 }
